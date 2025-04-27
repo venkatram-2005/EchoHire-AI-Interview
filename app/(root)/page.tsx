d88c839924ad1f9d55499ae372992c9219f1a363
Valluri Venkatram <vallurivenkatram245913@gmail.com>
2025-04-27 14:58:29 +0530

    setup app, auth pages, routing and structure

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      HomePage
+    </div>
+  )
+}
+
+export default page
---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "sonner";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "EchoHire",
+  description: "An AI powered platform for preparing for interviews",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${monaSans.className} antialiased pattern`}
+      >
+        {children}
+        <Toaster/>
+      </body>
+    </html>
+  );
+}
