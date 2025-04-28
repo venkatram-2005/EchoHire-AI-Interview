bd7fd5f48f484186d639b2e007a83c3ca392c64f
Valluri Venkatram <vallurivenkatram245913@gmail.com>
2025-04-28 07:12:14 +0530

    added img domain

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,10 @@ const nextConfig: NextConfig = {
   }, 
   typescript:{
     ignoreBuildErrors:true,
-  }
+  },
+  images: {
+    domains: ["cdn.jsdelivr.net"],
+  },
 };
 
 export default nextConfig;
