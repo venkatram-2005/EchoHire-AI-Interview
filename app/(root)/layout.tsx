2360b4da183cfcff09b984d5ee5dda7a92de4a47
Valluri Venkatram <vallurivenkatram245913@gmail.com>
2025-04-28 07:06:20 +0530

    Added Log Out

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from "react";
 import { redirect } from "next/navigation";
 
 import { isAuthenticated } from "@/lib/actions/auth.action";
+import { signOut } from "@/lib/actions/auth.action";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
@@ -11,11 +12,34 @@ const Layout = async ({ children }: { children: ReactNode }) => {
 
   return (
     <div className="root-layout">
-      <nav>
+      <nav className="flex items-center justify-between p-4">
+        {/* Logo */}
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
           <h2 className="text-primary-100">EchoHire</h2>
         </Link>
+
+        {/* Right side buttons */}
+        <div className="flex items-center gap-4">
+          {isUserAuthenticated ? (
+            // If signed in
+            <form action={signOut}>
+              <button type="submit" className="btn btn-primary">
+                Logout
+              </button>
+          </form>
+          ) : (
+            // If not signed in
+            <>
+              <Link href="/sign-in" className="btn btn-outline">
+                Sign In
+              </Link>
+              <Link href="/sign-up" className="btn btn-primary">
+                Sign Up
+              </Link>
+            </>
+          )}
+        </div>
       </nav>
 
       {children}
